0499caf0573a46e5ebc2ea52d6b1dea9ead87bad
w91379137 <w913791379137@gmail.com>
2022-11-02 15:54:51 +0800

    [feat] 新增 lobby 畫面 ng g c page/Lobby

--- frontend/AngularGui/src/app/app-routing.module.ts
+++ frontend/AngularGui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LobbyComponent } from './page/lobby/lobby.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LobbyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
