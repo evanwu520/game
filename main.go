1d9850966f658d6c77a561183cf2887e86008b19
evan <evanwu520.gamil.com>
2022-11-04 14:57:46 +0800

    add debug path

--- main.go
+++ main.go
@@ -16,14 +16,14 @@ var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+	// if r.URL.Path != "/" {
+	// 	http.Error(w, "Not found", http.StatusNotFound)
+	// 	return
+	// }
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	// 	return
+	// }
 	http.ServeFile(w, r, "home.html")
 }
 
@@ -51,12 +51,12 @@ func main() {
 
 	http.HandleFunc("/start", gameManager.Start)
 
-	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/debug", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 
-	// http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
